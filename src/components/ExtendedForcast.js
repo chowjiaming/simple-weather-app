18b4191dc8dca52b2cf6a8eddf0a1dce1891f830
joefstack <joefstack@gmail.com>
2021-01-12 14:13:09 -0700

    added error handling

--- src/components/ExtendedForcast.js
+++ src/components/ExtendedForcast.js
@@ -1,25 +1,30 @@
 import React from "react";
 
 export default function ExtendedForcast({ forcast, isLoading, error }) {
-  let temperatureMessage;
+  let temperatureContent, iconContent;
+  if (isLoading) {
+    temperatureContent = <p>Loading...</p>;
+    iconContent = <p>Loading...</p>;
+  } else if (error) {
+    temperatureContent = <p>Error!</p>;
+    iconContent = <p>Error!</p>;
+  } else {
+    temperatureContent = (
+      <p className="temperature-extended">{forcast.main.temp}°</p>
+    );
+    iconContent = (
+      <img
+        className="weather-icon"
+        src={`https://openweathermap.org/img/w/${forcast.weather[0].icon}.png`}
+        alt={`Icon Code ${forcast.weather[0].icon}`}
+      />
+    );
+  }
   return (
     <div className="extended-forcast">
       <p className="day">Today</p>
-      {!isLoading ? (
-        <img
-          className="weather-icon"
-          src={`https://openweathermap.org/img/w/${forcast.weather[0].icon}.png`}
-          alt={`Icon Code ${forcast.weather[0].icon}`}
-        />
-      ) : (
-        <p>Loading...</p>
-      )}
-      {temperatureMessage}
-      {!isLoading ? (
-        <p className="temperature-extended">{forcast.main.temp}°</p>
-      ) : (
-        <p>Loading...</p>
-      )}
+      {iconContent}
+      {temperatureContent}
     </div>
   );
 }
--- src/components/Today.js
+++ src/components/Today.js
@@ -1,30 +1,35 @@
 import React from "react";
 
 export default function Today({ forcast, isLoading, error }) {
+  // Content variables to display content based on parameters
+  let temperatureContent, iconContent, descriptionContent;
+  if (isLoading) {
+    temperatureContent = <p>Loading...</p>;
+    iconContent = <p>Loading...</p>;
+    descriptionContent = <p>Loading</p>;
+  } else if (error) {
+    temperatureContent = <p>Error!</p>;
+    iconContent = <p>Error!</p>;
+    descriptionContent = <p>Error</p>;
+  } else {
+    temperatureContent = <p className="temperature">{forcast.main.temp}°</p>;
+    iconContent = (
+      <img
+        className="weather-icon"
+        src={`https://openweathermap.org/img/w/${forcast.weather[0].icon}.png`}
+        alt={`Icon Code ${forcast.weather[0].icon}`}
+      />
+    );
+    descriptionContent = <p className="temperature">{forcast.main.temp}°</p>;
+  }
   return (
     <div className="today-forcast">
       <p className="date">Today</p>
       {/* Display loading while data is fetching */}
-      {!isLoading ? (
-        <img
-          className="weather-icon"
-          src={`https://openweathermap.org/img/w/${forcast.weather[0].icon}.png`}
-          alt={`Icon Code ${forcast.weather[0].icon}`}
-        />
-      ) : (
-        <p>Loading...</p>
-      )}
+      {iconContent}
       <div className="weather-description">
-        {!isLoading ? (
-          <p className="temperature">{forcast.main.temp}°</p>
-        ) : (
-          <p>Loading...</p>
-        )}
-        {!isLoading ? (
-          <p className="description">{forcast.weather[0].description}</p>
-        ) : (
-          <p>Loading...</p>
-        )}
+        {temperatureContent}
+        {descriptionContent}
       </div>
     </div>
   );
