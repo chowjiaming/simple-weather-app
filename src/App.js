9360a0ccd4919ab199c7a272f1e34bada9275c48
joefstack <joefstack@gmail.com>
2021-01-12 09:19:38 -0700

    added api key to env file and custom fetch hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
+import { useState } from "react";
+// The css is preprocessed by my package with my development script
+import "./App.css";
+// My custom fetch API hook
+import useFetchWeather from "./helpers/useFetchWeather";
 
 function App() {
+  // Set default city to Ottawa when the app starts
+  const [city, setCity] = useState("Ottawa");
+  // Grabbing app params from my custom hook
+  const { forcast, isLoading, error } = useFetchWeather(city);
+
+  console.log(forcast, isLoading, error);
   return (
     <div className="App">
       <h1>Weather App</h1>
---
+++ src/helpers/useFetchWeather.js
@@ -0,0 +1,57 @@
+import { useReducer, useEffect } from "react";
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
+// Actions for fetching and receiving data
+const ACTIONS = {
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    // Set loading to true and clearing previous forcast data on data request
+    case ACTIONS.MAKE_REQUEST:
+      return { isLoading: true, forcast: {} };
+    // Loading forcast into state and setting loading to false
+    case ACTIONS.GET_DATA:
+      return { isLoading: false, forcast: action.payload };
+    case ACTIONS.ERROR:
+      return {
+        isLoading: false,
+        error: action.payload.error,
+        forcast: {},
+      };
+    default:
+      return state;
+  }
+}
+
+export default function useFetchWeather(city) {
+  const [state, dispatch] = useReducer(reducer, {
+    forcast: {},
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    const fetchNewData = () => {
+      dispatch({ type: ACTIONS.MAKE_REQUEST });
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch({
+            type: ACTIONS.GET_DATA,
+            payload: data,
+          });
+        })
+        .catch((error) => {
+          dispatch({ type: ACTIONS.ERROR, payload: { error } });
+        });
+    };
+    fetchNewData();
+  }, [city]);
+
+  return state;
+}
